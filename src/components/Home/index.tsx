4dd2c4484e8adb517d620850480fd66e8ad1a7bf
Luke Skywalker <skywalker.eth@protonmail.com>
2023-02-14 15:42:07 +0530

    Init the project with Next, Tailwaind, Prettier, TS

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+    
+      </main>
+    </div>
+  )
+}
+
+export default Home
