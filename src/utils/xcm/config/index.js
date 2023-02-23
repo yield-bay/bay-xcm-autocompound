eb24e1a33c5d107dc6eec823b1ca72f2412f7e04
Jack Sparrow <capjacksparrow42@protonmail.com>
2023-02-23 15:42:46 +0530

    xcm

---
+++ src/utils/xcm/config/index.js
@@ -0,0 +1,21 @@
+import RococoDev from './rococo-dev';
+import Rococo from './rococo';
+
+import MangataDev from './mangata-dev';
+import MangataRococo from './mangata-rococo';
+import Mangata from './mangata';
+
+import Shibuya from './shibuya';
+import Rocstar from './rocstar';
+import Shiden from './shiden';
+
+import TuringDev from './turing-dev';
+import TuringStaging from './turing-staging';
+import Turing from './turing';
+
+export {
+    Rococo, RococoDev,
+    MangataDev, MangataRococo, Mangata,
+    TuringDev, Turing, TuringStaging,
+    Shibuya, Rocstar, Shiden,
+};
