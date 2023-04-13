1066d3b4a02c360b67a7310e9891763466e4519d
Luke Skywalker <skywalker.eth@protonmail.com>
2023-04-13 20:46:58 +0530

    fixed the extra step to connect wallet. improved flow.

--- src/utils/polkadotMethods.ts
+++ src/utils/polkadotMethods.ts
@@ -3,15 +3,21 @@ import type { Wallet, WalletAccount } from '@talismn/connect-wallets';
 
 export const pullWalletAccounts = async (
   wallet: Wallet,
-  setWalletAccounts: (value: WalletAccount[]) => void
+  setWalletAccounts: (value: WalletAccount[]) => void,
+  setAccount: any,
+  setIsOpen: any
 ) => {
   try {
     await wallet.enable(APP_NAME);
     await wallet.subscribeAccounts((accounts: WalletAccount[] | undefined) => {
       // jotai:: setting accounts in selected wallet
       setWalletAccounts(accounts as WalletAccount[]);
+      if(accounts?.length == 1) {
+        setAccount(accounts[0]);
+        setIsOpen(false);
+      }      
     });
   } catch (err) {
-    console.error(err);
+    console.log('Error in subscribing accounts: ', err);
   }
 };
